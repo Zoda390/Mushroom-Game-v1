5c0a010aff5c5d68c32a3de8d4545dcfe39ac9ee
Zoda390 <81502387+Zoda390@users.noreply.github.com>
2022-10-17 18:23:35 -0400

    2.5d world + multiplayer walking

--- public/client.js
+++ public/client.js
@@ -1,7 +1,7 @@
 var channel;
-var players = [];
-var pos;
-const speed = 3;
+var tile_map = [];
+var tileSize = 32;
+var player = {x: 0, y: 0, z: 2};
 
 function setup(){
     channel = geckos({ port: 3000 })
@@ -12,60 +12,72 @@ function setup(){
             return
         }
 
-        channel.on('update', data => {
-            players = data;
+        channel.on('give_world', data => {
+            tile_map = data;
         })
 
-        channel.emit('join', {x: random(0, width), y: random(0, height), id: channel.id})
+        channel.on('change', data => {
+            tile_map[data.y][data.x][data.z] = data.to;
+        })
+
+        channel.emit('join', {x: 0, y: 0, z: 2, id: channel.id})
     })
 
-    createCanvas(400, 400);
+    createCanvas(1200, 700);
 }
 
 function draw(){
     background(220);
-    console.log(players);
-    for(let i = 0; i < players.length; i++){
-        fill(255, 0, 0);
-        if(players[i].id === channel.id){
-            fill(255, 255, 0);
+    for(let y = 0; y < tile_map.length; y++){
+        for(let x = 0; x < tile_map[y].length; x++){
+          for(let z = 0; z < tile_map[y][x].length; z++){
+            let color = {r: 0, g: 0, b: 0, a: 0};
+            if(tile_map[y][x][z] == 1){
+                color = {r: 100, g: 100, b: 100, a: 255};
+            }
+            else if(tile_map[y][x][z] == 2){
+                color = {r: 0, g: 255, b: 0, a: 255};
+            }
+            else if(tile_map[y][x][z] == 3){
+                color = {r: 0, g: 255, b: 255, a: 70};
+            }
+            else if(tile_map[y][x][z] == 4){
+                color = {r: 255, g: 0, b: 0, a: 255};
+            }
+            let shadow = 50;
+            strokeWeight(2);
+            stroke(color.r - shadow, color.g - shadow, color.b - shadow, color.a)
+            fill(color.r, color.g, color.b, color.a);
+            rect(((x+1)*tileSize)+(x+1), ((y+2)*tileSize) - ((z+1) * tileSize/2) - z, tileSize, tileSize)
+            stroke(color.r - shadow*2, color.g - shadow*2, color.b - shadow*2, color.a)
+            fill(color.r - shadow, color.g - shadow, color.b - shadow, color.a);
+            rect(((x+1)*tileSize)+(x+1), ((y+2)*tileSize) - z - ((z+1) * tileSize/2) + tileSize, tileSize, tileSize/2)
+          }
         }
-        circle(players[i].x, players[i].y, 10);
-    }
-    
+      }
 
     takeInput();
 }
 
 function takeInput(){
-    let player;
-    for(let i = 0; i < players.length; i++){
-        if(players[i].id === channel.id){
-            player = players[i];
-        }
+    if (keyIsDown(LEFT_ARROW) && player.x != 0) {
+        channel.emit('change', {x: player.x, y: player.y, z: player.z, to: 0});
+        player.x -= 1;
+        channel.emit('change', {x: player.x, y: player.y, z: player.z, to: 4});
     }
-    let change = false;
-    if (keyIsDown(LEFT_ARROW)) {
-        player.x -= speed;
-        change = true;
+    if (keyIsDown(RIGHT_ARROW) && player.x != tile_map[0].length-1) {
+        channel.emit('change', {x: player.x, y: player.y, z: player.z, to: 0});
+        player.x += 1;
+        channel.emit('change', {x: player.x, y: player.y, z: player.z, to: 4});
     }
-    
-    if (keyIsDown(RIGHT_ARROW)) {
-        player.x += speed;
-        change = true;
+    if (keyIsDown(UP_ARROW) && player.y != 0) {
+        channel.emit('change', {x: player.x, y: player.y, z: player.z, to: 0});
+        player.y -= 1;
+        channel.emit('change', {x: player.x, y: player.y, z: player.z, to: 4});
     }
-    
-    if (keyIsDown(UP_ARROW)) {
-        player.y -= speed;
-        change = true;
-    }
-    
-    if (keyIsDown(DOWN_ARROW)) {
-        player.y += speed;
-        change = true;
-    }
-
-    if(change){
-        channel.emit('move', player);
+    if (keyIsDown(DOWN_ARROW) && player.y != tile_map.length-1) {
+        channel.emit('change', {x: player.x, y: player.y, z: player.z, to: 0});
+        player.y += 1;
+        channel.emit('change', {x: player.x, y: player.y, z: player.z, to: 4});
     }
 }
