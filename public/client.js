d5a5f194ea0b3f1082fb505d5f862342913cde71
Christian Rodriguez <christiantrodriguez3@gmail.com>
2022-10-19 06:14:47 -0400

    added better player imgs, added shadows, added flying, wasd controls, added running, upgraded map creation

--- public/client.js
+++ public/client.js
@@ -1,7 +1,17 @@
 var channel;
-var tile_map = [];
-var tileSize = 32;
-var player = {x: 0, y: 0, z: 2};
+var tile_map;
+var tileSize = 64;
+var player = {x: 0, y: 0, z: 5, id: 0};
+
+var img_map = [];
+function preload(){
+    img_map.push(0);
+    img_map.push(loadImage("imgs/stone-v1.png"));
+    img_map.push(loadImage("imgs/grass-v1.png"));
+    img_map.push(loadImage("imgs/water-v1.png"));
+    img_map.push(loadImage("imgs/player-v1.png"));
+    img_map.push(loadImage("imgs/player2-v1.png"));
+}
 
 function setup(){
     channel = geckos({ port: 3000 })
@@ -16,6 +26,12 @@ function setup(){
             tile_map = data;
         })
 
+        channel.on('update_id', data => {
+            if(player.id === 0){
+                player.id = data;
+            }
+        })
+
         channel.on('change', data => {
             tile_map[data.y][data.x][data.z] = data.to;
         })
@@ -23,38 +39,44 @@ function setup(){
         channel.emit('join', {x: 0, y: 0, z: 2, id: channel.id})
     })
 
-    createCanvas(1200, 700);
+    createCanvas(tileSize*30, tileSize*14);
 }
 
 function draw(){
-    background(220);
-    for(let y = 0; y < tile_map.length; y++){
-        for(let x = 0; x < tile_map[y].length; x++){
-          for(let z = 0; z < tile_map[y][x].length; z++){
-            let color = {r: 0, g: 0, b: 0, a: 0};
-            if(tile_map[y][x][z] == 1){
-                color = {r: 100, g: 100, b: 100, a: 255};
+    background(139, 176, 173);
+    if(tile_map != undefined){
+        translate((-player.x*tileSize)+(tileSize*15), (-player.y*tileSize)+(tileSize*7));
+        push();
+        for(let y = 0; y < tile_map.length; y++){
+            for(let x = 0; x < tile_map[y].length; x++){
+                for(let z = 0; z < tile_map[y][x].length; z++){
+                    imageMode(CORNER);
+                    if(tile_map[y][x][z] != 0){
+                        if(tile_map[y][x][z] === 4){
+                            fill(0, 70);
+                            let shadowZ = 0;
+                            for(let z2 = 0; z2 < tile_map[y][x].length; z2++){
+                                if(tile_map[y][x][z2] != 0 && tile_map[y][x][z2] != 4){
+                                    shadowZ = z2;
+                                }
+                            }
+                            circle((x*tileSize) + (tileSize/2), (y*tileSize) - (shadowZ * tileSize/2) + (tileSize/2), (10-(z-shadowZ)) * (tileSize/20));
+                            if(player.x === x && player.y === y && player.z === z){
+                                image(img_map[(tile_map[y][x][z])], (x*tileSize), (y*tileSize) - (z * tileSize/2), tileSize, tileSize + (tileSize/2));
+                            }
+                            else{
+                                image(img_map[(tile_map[y][x][z])+1], (x*tileSize), (y*tileSize) - (z * tileSize/2), tileSize, tileSize + (tileSize/2));
+                            }
+                        }
+                        else{
+                            image(img_map[(tile_map[y][x][z])], (x*tileSize), (y*tileSize) - (z * tileSize/2), tileSize, tileSize + (tileSize/2));
+                        }
+                    }
+                }
             }
-            else if(tile_map[y][x][z] == 2){
-                color = {r: 0, g: 255, b: 0, a: 255};
-            }
-            else if(tile_map[y][x][z] == 3){
-                color = {r: 0, g: 255, b: 255, a: 70};
-            }
-            else if(tile_map[y][x][z] == 4){
-                color = {r: 255, g: 0, b: 0, a: 255};
-            }
-            let shadow = 50;
-            strokeWeight(2);
-            stroke(color.r - shadow, color.g - shadow, color.b - shadow, color.a)
-            fill(color.r, color.g, color.b, color.a);
-            rect(((x+1)*tileSize)+(x+1), ((y+2)*tileSize) - ((z+1) * tileSize/2) - z, tileSize, tileSize)
-            stroke(color.r - shadow*2, color.g - shadow*2, color.b - shadow*2, color.a)
-            fill(color.r - shadow, color.g - shadow, color.b - shadow, color.a);
-            rect(((x+1)*tileSize)+(x+1), ((y+2)*tileSize) - z - ((z+1) * tileSize/2) + tileSize, tileSize, tileSize/2)
-          }
         }
-      }
+        pop();
+    }
 
     takeInput();
 }
@@ -63,10 +85,12 @@ var move_right_button = 68; //d
 var move_left_button = 65; //a
 var move_up_button = 87; //w
 var move_down_button = 83; //s
+var move_fly_up_button = 81; //q
+var move_fly_down_button = 69; //e
 var run_button = 16; //shift
 var lastmoveMilli = 0;
 var move_wait = 100;
-var run_wait = 50;
+var run_wait = 40;
 function takeInput(){
     if (keyIsDown(move_right_button) && player.x != tile_map[0].length-1) {
         if (millis() - lastmoveMilli > ((keyIsDown(run_button)) ? run_wait:move_wait)) {
@@ -92,11 +116,26 @@ function takeInput(){
             lastmoveMilli = millis();
         }
     }
-
     if (keyIsDown(move_down_button) && player.y != tile_map.length-1) {
         if (millis() - lastmoveMilli > ((keyIsDown(run_button)) ? run_wait:move_wait)) {
             channel.emit('change', {x: player.x, y: player.y, z: player.z, to: 0});
             player.y += 1;
+            channel.emit('change', {x: player.x, y: player.y, z: player.z, to: 4});
+            lastmoveMilli = millis();
+        }
+    }
+    if (keyIsDown(move_fly_up_button) && player.z != 1) {
+        if (millis() - lastmoveMilli > ((keyIsDown(run_button)) ? run_wait:move_wait)) {
+            channel.emit('change', {x: player.x, y: player.y, z: player.z, to: 0});
+            player.z -= 1;
+            channel.emit('change', {x: player.x, y: player.y, z: player.z, to: 4});
+            lastmoveMilli = millis();
+        }
+    }
+    if (keyIsDown(move_fly_down_button) && player.z != tile_map[0][0].length-1) {
+        if (millis() - lastmoveMilli > ((keyIsDown(run_button)) ? run_wait:move_wait)) {
+            channel.emit('change', {x: player.x, y: player.y, z: player.z, to: 0});
+            player.z += 1;
             channel.emit('change', {x: player.x, y: player.y, z: player.z, to: 4});
             lastmoveMilli = millis();
         }
