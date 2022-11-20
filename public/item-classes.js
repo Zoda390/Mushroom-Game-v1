60045a5e0c039fca48af5b00292da77de0df4c09
Zoda390 <81502387+Zoda390@users.noreply.github.com>
2022-11-20 13:18:56 -0500

    added item pngs

--- public/item-classes.js
+++ public/item-classes.js
@@ -2,13 +2,12 @@ var item_type_map = [0, 'block', 'tool', 'consumable'];
 var item_name_map = [0, 'stone', 'grass', 'water', 'wood', 'pickaxe'];
 
 class ClientItem{ //block
-    constructor(type, name, amount, left, right){
+    constructor(type, name, amount, click){
         this.type = type;
         this.name = name;
         this.img_num = find_in_array(this.name, tile_name_map); //img found in img_map
         this.amount = amount;
-        this.left = left;
-        this.right = right;
+        this.click = click;
     }
 
     toStr(){
@@ -19,14 +18,8 @@ class ClientItem{ //block
         image(img_map[this.img_num], x, y);
     }
 
-    L_click(){
-        let txtinput = this.left;
-        let script = tokenize(txtinput);
-        parse(script);
-    }
-
-    R_click(){
-        let txtinput = this.right;
+    clicked(){
+        let txtinput = this.click;
         let script = tokenize(txtinput);
         parse(script);
     }
